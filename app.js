b3da983f7dc8b0950297091dc838fb5a114e7ec9
Rajika <inbox.rajika@gmail.com>
2015-06-07 12:50:51 +0530

    Added router module

--- app.js
+++ app.js
@@ -3,27 +3,12 @@ var app = express();
 var Movie = require('./models/movieModel.js');
 var port = 3000;
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/movies');
+var db = mongoose.connect('mongodb://localhost/mymovies');
 
-
-var movieRouter = express.Router();
-movieRouter.route('/myMovies')
-	.get(function(req,res){
-		if(req.query.genre){
-			var query = req.query;	
-		}
-		Movie.find(query,function(err,movies){
-			if(err)
-				console.error(err);
-			else
-				res.json(movies);
-		});
-	});
+movieRouter = require('./routes/movieRoutes.js')(Movie);
 
 
 app.use('/Movies',movieRouter);
-
-
 
 app.get('/',function(req,res){
 	res.send('Hello');
---
+++ routes/movieRoutes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var routes = function(Movie){
+	var movieRouter = express.Router();
+
+	movieRouter.route('/myMovies')
+	.get(function(req,res){
+		if(req.query.genre || req.query.title){
+			var query = req.query;	
+		}
+		Movie.find(query,function(err,movies){
+			if(err)
+				console.error(err);
+			else
+				res.json(movies);
+		});
+	});
+
+	movieRouter.route('/myMovies/:title')
+	.get(function(req,res){
+		Movie.find({ title : req.params.title },function(err,movies){
+			if(err) throw err;
+
+			res.json(movies);
+		});
+	});
+	return movieRouter;
+};
+
+module.exports = routes;
