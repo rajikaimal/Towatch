d4243439d7a5a655d4469270c6249363b09b4eaf
Rajika <inbox.rajika@gmail.com>
2015-12-12 13:43:22 +0530

    Add static data

---
+++ models/data.js
@@ -0,0 +1,22 @@
+var movies = [
+	{
+		"id": 1,
+		"name": "Transformers",
+		"genre": "Action",
+		"watched": "true"
+	},
+	{
+		"id": 2,
+		"name": "TMNT",
+		"genre": "Action",
+		"watched": "false"
+	},
+	{
+		"id": 3,
+		"name": "Batman",
+		"genre": "Comic movie",
+		"watched": "false"
+	}
+];
+
+module.exports = movies;
